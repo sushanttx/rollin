8fe8f9548aeb4dc8c54e1dc06758c7e0071f6fbc
sushanttx <sushant.langhi05@gmail.com>
2024-07-21 01:11:13 +0530

    Initial commit

---
+++ theyseeme/src/main/java/com/theyhatin/theyseemerollin/TheyhatinApplication.java
@@ -0,0 +1,13 @@
+package com.theyhatin.theyseemerollin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheyhatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheyhatinApplication.class, args);
+	}
+
+}
---
+++ theyseeme/src/test/java/com/theyhatin/theyseemerollin/TheyhatinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyhatin.theyseemerollin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheyhatinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
